bf97b443d1a98078ed4eeab95e1eab1a4cc21097
Florian Gilcher <florian.gilcher@asquera.de>
2012-03-26 01:33:47 +0200

    Fix downloader to follow redirects

--- fetch_es.sh
+++ fetch_es.sh
@@ -1,4 +1,5 @@
 rm -rf elasticsearch # remove old version
-curl https://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-$ES_VERSION.tar.gz -o elasticsearch.tar.gz
+curl https://github.com/downloads/elasticsearch/elasticsearch/elasticsearch-$ES_VERSION.tar.gz -L -o elasticsearch.tar.gz
 tar xvf elasticsearch.tar.gz
+mv elasticsearch-$ES_VERSION elasticsearch
 rm elasticsearch.tar.gz
